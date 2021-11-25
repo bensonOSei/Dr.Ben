f7ea329f2623da62e1ade3ce5fbef2c51c78e156
bensonOSei <kbenson643@gmail.com>
2021-11-25 03:21:57 +0000

    Update to UI/UX

--- js/app.js
+++ js/app.js
@@ -6,3 +6,27 @@ function closeModal(){
     document.getElementById("direct-modal").style.height = "0%"
 }
 
+function openDetailsModal(){
+    document.getElementById("det-modal").style.height = "100%"
+}
+
+var acc = document.getElementsByClassName("modal-head");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
+function closeDet(){
+    let modal = document.getElementById("det-modal");
+
+    modal.style.height = '0'
+}

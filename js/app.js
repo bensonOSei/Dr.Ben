a3daa04c026cd77022b724c43383a7db38debcd2
bensonOSei <kbenson643@gmail.com>
2021-11-25 01:38:55 +0000

    First Commit

---
+++ js/app.js
@@ -0,0 +1,8 @@
+function openModal(){
+    document.getElementById("direct-modal").style.height = "100%"
+}
+
+function closeModal(){
+    document.getElementById("direct-modal").style.height = "0%"
+}
+
